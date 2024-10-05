3c963d0d8f3594e0cc840b9f5380c9ac2d91f27f
amphetaminefather <158784347+Silene0259@users.noreply.github.com>
2024-10-05 09:32:29 -0700

    Added Initial of XChaCha20

--- src/sym_encryption/chacha20.rs
+++ src/sym_encryption/chacha20.rs
@@ -1,11 +1,17 @@
 use chacha20poly1305::*;
 use new_rand::*;
 
+use chacha20poly1305::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    ChaCha20Poly1305, Nonce
+};
 pub struct ChaCha20Encrypt;
+pub struct ChaCha20Decrypt;
 
 impl ChaCha20Encrypt {
     pub fn new<T: AsRef<[u8]>>(key: T) {
-        let nonce = chacha20poly1305::XChaCha20Poly1305::generate_nonce(thread_rng());
-        let cipher = XChaCha20Poly1305::new(key.as_ref());
+        let nonce = chacha20poly1305::XChaCha20Poly1305::generate_nonce(&mut OsRng);
+        let gen_key = ChaCha20Poly1305::generate_key(&mut OsRng);
+        let cipher = XChaCha20Poly1305::new(key.as_ref().into());
     }
 }
